2383539f6257c1cad10bba98d75922cac90589be
Rashmi Agarwal <rashmiagar@gmail.com>
2015-10-15 17:34:55 +0530

    Added restrictions for image dimension, size and type validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,20 @@
 class Product < ActiveRecord::Base
   belongs_to :store
   mount_uploader :image, ImageUploader
+  attr_accessor :image_width, :image_height
+  validate :check_avatar_dimensions
+
+  validate :image_size_validation, :if => "image?"
+
+  def image_size_validation
+    if image.size > 1.megabytes
+      errors.add(:base, "Image should be less than 1MB")
+    end
+  end
+
+  def check_avatar_dimensions
+    if !image_cache.nil? && (image.width < 300 || image.height < 300)
+    errors.add :image, "Dimension too small."
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,50 +13,53 @@
 
 ActiveRecord::Schema.define(version: 20151014100849) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "body",       limit: 65535
-    t.integer  "user_id",    limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "products", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.string   "image",       limit: 255
-    t.text     "product_url", limit: 65535
-    t.integer  "store_id",    limit: 4
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "title"
+    t.string   "image"
+    t.text     "product_url"
+    t.integer  "store_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "products", ["store_id"], name: "index_products_on_store_id", using: :btree
 
   create_table "stores", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.integer  "user_id",    limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "stores", ["user_id"], name: "index_stores_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name",                   limit: 255
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
